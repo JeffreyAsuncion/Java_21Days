3df1f7aa48dd761105ebfe648b9394d871078992
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-25 07:05:15 -0400

    Simple Arithmetic

---
+++ Day03/ArithmeticTest.java
@@ -0,0 +1,20 @@
+public class ArithmeticTest {
+
+    public static void main(String[] args) {
+     
+        short x = 6;
+	int y = 4;
+	float a = 12.5f;
+	float b = 7f;
+
+	System.out.println("x is " + x + ", y is " + y);
+
+	System.out.println("x + y =  " +  (x + y));
+	System.out.println("x - y =  " +  (x - y));
+	System.out.println("x / y =  " +  (x / y));
+	System.out.println("x % y =  " + (x % y));
+
+	System.out.println("a is " + a + ", b is " + b);
+	System.out.println("a / b = "+  (a / b));
+    }
+}
