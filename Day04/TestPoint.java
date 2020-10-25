ed5884cee4eeb953c905ed8fdeb0b2170ab572b6
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-25 11:07:31 -0400

    TestPoint

---
+++ Day04/TestPoint.java
@@ -0,0 +1,20 @@
+import java.awt.Point;
+
+public class TestPoint {
+
+    public static void main(String[] args) {
+    
+        Point thePoint = new Point(10, 10);
+
+	System.out.println("X is " + thePoint.x);
+	System.out.println("Y is " + thePoint.y);
+
+	System.out.println("Setting X to 5.");
+	thePoint.x = 5;
+	System.out.println("Setting y to 15.");
+	thePoint.y = 15;
+
+	System.out.println("X is " + thePoint.x);
+	System.out.println("Y is " + thePoint.y);
+    }
+}
