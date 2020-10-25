4f70de5e6880be953d990bc3f5b0f069f15273e6
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-25 16:58:53 -0400

    test array and ifelse

---
+++ Day05/CreateArrays.java
@@ -0,0 +1,12 @@
+public class CreateArrays {
+
+    public static void main(String[] args) {
+        int i; 
+        String[] chiles = {"jalapeno", "anaheim", "serrano"};
+        
+	for(i=0; i < 3; i++) {
+	    System.out.println(i);
+	    System.out.println(chiles[i]);
+	} 
+    }
+}
---
+++ Day05/TestIfElse.java
@@ -0,0 +1,17 @@
+public class TestIfElse {
+
+    public static void main(String[] args) {
+    
+        int x, y;
+
+	x = 10;
+	y = 20;
+
+	if (x < y) {
+	    System.out.println("x is smaller than y");
+	}
+	else {
+	    System.out.println("y is bigger.");
+	}
+    }
+} 
