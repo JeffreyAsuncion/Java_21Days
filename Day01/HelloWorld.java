28c5185fb3d0328506acd5a260a795cee929d240
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-25 05:55:48 -0400

    HelloWorld

---
+++ Day01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {        // public not in book, but it still works
+
+    public static void main(String[] args) {  // (String args[]), in book but still works
+
+         System.out.println("Hello World!");
+    }
+}
