a28608f0763ba3554a8d1c6e7c4867d00fd0023c
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-25 21:45:27 -0400

    DoWhileLoop test

---
+++ Day05/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+    
+        int x = 1;
+
+	do {
+	    System.out.println("Looping, round " + x);
+	    x++;
+	} while (x <= 10);
+    
+    }
+}
